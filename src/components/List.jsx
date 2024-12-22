afec8d0b441cf35d6e96e1a9478f88f37d3d2b70
VlachakisTheodoros <vlahakis.gr@gmail.com>
2024-12-22 15:09:21 +0200

    Central allignment both for 'price' on Products page and for 'role' on Employees page

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -145,7 +145,8 @@ const List = ({
                     >
                         {columns.map((col, colIndex) => (
                             <p
-                                className={`w-full ${col === 'zip' || col === 'quantity' || col ==='warehouseID' ? 'text-center' : 'text-start'} px-2 truncate mx-2`}
+                                className={`w-full ${col === 'zip' || col === 'quantity' || col ==='warehouseID'
+                                    || col === 'role' || col === 'price' ? 'text-center' : 'text-start'} px-2 truncate mx-2`}
                                 key={colIndex}
                             >
                                 {item[col]}

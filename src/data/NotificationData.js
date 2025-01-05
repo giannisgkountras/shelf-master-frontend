1587aa6a6c4abbb91fe1c1795c5c71549760ba12
giannisgkountras <gkountrasioannis@gmail.com>
2025-01-05 21:40:47 +0200

    Made notifications 3 instead of 4

--- src/data/NotificationData.js
+++ src/data/NotificationData.js
@@ -1,32 +1,31 @@
-import attendanceApi from "../api/attendance";
+import attendanceApi from '../api/attendance';
 
 const fetchNotificationsData = async () => {
     const attendances = await attendanceApi.getAll();
-    
+
     const notificationsAttendances = attendances.map((att) => ({
-        employeeRole: att.role === 'Admin' ? 'Admin': 'Employee',
-        color: att.role === 'Admin' ? 'green': 'softYellow',
-        message: 
-            `The employee ${att.fullName} ${att.status === 1 ? 'checked-in': 'checked-out'}
+        employeeRole: att.role === 'Admin' ? 'Admin' : 'Employee',
+        color: att.role === 'Admin' ? 'green' : 'softYellow',
+        message: `The employee ${att.fullName} ${att.status === 1 ? 'checked-in' : 'checked-out'}
              of warehouse ${att.warehouseID}.`,
         timestamp: new Date(att.timestamp).toLocaleString([], {
             year: 'numeric',
             month: '2-digit',
-            day:'2-digit',
+            day: '2-digit',
             hour: '2-digit',
             minute: '2-digit',
-            hour12: false
+            hour12: false,
         }),
     }));
 
-    if (notificationsAttendances.length < 4) {
+    if (notificationsAttendances.length < 3) {
         return { notificationsAttendances, extraNotificationsData: [] };
-    };
+    }
 
-    const notificationsData = notificationsAttendances.slice(0, 4);
-    const extraNotificationsData = notificationsAttendances.slice(4);
+    const notificationsData = notificationsAttendances.slice(0, 3);
+    const extraNotificationsData = notificationsAttendances.slice(3);
 
-    return {notificationsData, extraNotificationsData};
+    return { notificationsData, extraNotificationsData };
 };
 
 export default fetchNotificationsData;
